ad679e289037fff88953942d5e2732086e62faae
Eric Vallee <eric_vallee@webificservices.com>
2023-06-19 11:28:12 -0400

    feat: INFRA-160 Added grpc notifications push

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Ferlab-Ste-Justine/gitsync/config"
 	"github.com/Ferlab-Ste-Justine/gitsync/filesystem"
 	"github.com/Ferlab-Ste-Justine/gitsync/git"
+	"github.com/Ferlab-Ste-Justine/gitsync/grpc"
 	"github.com/Ferlab-Ste-Justine/gitsync/logger"
 
 	"github.com/Ferlab-Ste-Justine/etcd-sdk/client"
@@ -58,31 +59,50 @@ func main() {
 
 	if diff.IsEmpty() {
 		log.Infof("[main] No update to apply")
-	} else {
-		log.Infof(
-			"[main] Applying update: %d inserts, %d updates and %d deletions",
-			len(diff.Inserts),
-			len(diff.Updates),
-			len(diff.Deletions),
-		)
+		return
+	}
+	
+	log.Infof(
+		"[main] Applying update: %d inserts, %d updates and %d deletions",
+		len(diff.Inserts),
+		len(diff.Updates),
+		len(diff.Deletions),
+	)
 
-		applyErr := filesystem.ApplyDiffToDirectory(
-			conf.Filesystem.Path,
-			diff,
-			filesystem.ConvertFileMode(conf.Filesystem.FilesPermission),
-			filesystem.ConvertFileMode(conf.Filesystem.DirectoriesPermission),
-		)
-		if applyErr != nil {
-			log.Errorf(applyErr.Error())
+	if len(conf.GrpcNotifications) > 0 {
+		err = func() error {
+			notifCli, err := grpc.ConnectToNotifEndpoints(conf.GrpcNotifications)
+			if err != nil {
+				return err
+			}
+
+			defer notifCli.Close()
+	
+			sendErr := notifCli.Send(diff)
+			return sendErr
+		}()
+		if err != nil {
+			log.Errorf(err.Error())
 			os.Exit(1)
 		}
+	}
 
-		if len(conf.NotificationCommand) > 0 {
-			cmdErr := cmd.ExecCommand(conf.NotificationCommand, conf.NotificationCommandRetries, log)
-			if cmdErr != nil {
-				log.Errorf(cmdErr.Error())
-				os.Exit(1)
-			}
+	applyErr := filesystem.ApplyDiffToDirectory(
+		conf.Filesystem.Path,
+		diff,
+		filesystem.ConvertFileMode(conf.Filesystem.FilesPermission),
+		filesystem.ConvertFileMode(conf.Filesystem.DirectoriesPermission),
+	)
+	if applyErr != nil {
+		log.Errorf(applyErr.Error())
+		os.Exit(1)
+	}
+
+	if len(conf.NotificationCommand) > 0 {
+		cmdErr := cmd.ExecCommand(conf.NotificationCommand, conf.NotificationCommandRetries, log)
+		if cmdErr != nil {
+			log.Errorf(cmdErr.Error())
+			os.Exit(1)
 		}
 	}
 }
